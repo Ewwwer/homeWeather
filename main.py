baae78a81cafaa325238fbd286504df409920008
Ewwwer <48954179+Ewwwer@users.noreply.github.com>
2020-06-10 14:43:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import requests, json
+import requests
+import json
 from datetime import datetime
 
 api_key = '30a0a1deb5b8512aa9b6789ef5f99624'
@@ -27,7 +28,16 @@ if x["currently"] != "404":
     #guardamos varlores diarios en z
     z = x["daily"]
     prev_dias = z["data"]
-   
+
+    #Intentamos meter funcion para semplificar legibilidad del codigo
+    # def previsiones(prev_dias = z["data"]):
+    #     temp_max_manana = (["temperatureHigh"])
+    #     temp_min_manana = (["temperatureLow"])
+    #     pres_manana = ["pressure"]
+    #     icon_manana = ["icon"]
+    # print(previsiones(prev_dias[5]))
+
+    #Día 1
     time_manana = prev_dias[2]["time"]
     #convertimos UNIX recibido a formato legible
     print(datetime.utcfromtimestamp(time_manana).strftime('%d / %m'))
@@ -45,6 +55,76 @@ if x["currently"] != "404":
     else:
         print(r'\u1F817')
 
+    #Día 2
+    time_pasado_manana = prev_dias[3]["time"]
+    #convertimos UNIX recibido a formato legible
+    print(datetime.utcfromtimestamp(time_pasado_manana).strftime('%d / %m'))
+
+    temp_max_pasado_manana = int(prev_dias[3]["temperatureHigh"])
+    temp_min_pasado_manana = int(prev_dias[3]["temperatureLow"])
+    pres_pasado_manana = prev_dias[3]["pressure"]
+    icon_pasadp_manana = prev_dias[3]["icon"]
+
+    #mostramos un icono para saber si la presión sube o baja respecto al día anterior
+    if pres_pasado_manana > pres_manana:
+        print(r'\u1F815')
+    elif pres_pasado_manana == pres_manana:
+        print('=')
+    else:
+        print(r'\u1F817')
+    
+     #Día 3
+    time_tres_prev = prev_dias[4]["time"]
+    #convertimos UNIX recibido a formato legible
+    print(datetime.utcfromtimestamp(time_tres_prev).strftime('%d / %m'))
+
+    temp_max_tres = int(prev_dias[4]["temperatureHigh"])
+    temp_min_tres = int(prev_dias[4]["temperatureLow"])
+    pres_tres = prev_dias[4]["pressure"]
+    icon_tres = prev_dias[4]["icon"]
+
+    #mostramos un icono para saber si la presión sube o baja respecto al día anterior
+    if pres_tres > pres_pasado_manana:
+        print(r'\u1F815')
+    elif pres_tres == pres_pasado_manana:
+        print('=')
+    else:
+        print(r'\u1F817')
+
+
+     #Día 4
+    time_cuatro_prev = prev_dias[5]["time"]
+    #convertimos UNIX recibido a formato legible
+    print(datetime.utcfromtimestamp(time_cuatro_prev).strftime('%d / %m'))
+
+    temp_max_cuatro = int(prev_dias[5]["temperatureHigh"])
+    temp_min_cuatro = int(prev_dias[5]["temperatureLow"])
+    pres_cuatro = prev_dias[5]["pressure"]
+    icon_cuatro = prev_dias[5]["icon"]
+
+    #mostramos un icono para saber si la presión sube o baja respecto al día anterior
+    if pres_cuatro > pres_tres:
+        print(r'\u1F815')
+    elif pres_cuatro == pres_tres:
+        print('=')
+    else:
+        print(r'\u1F817')
     
 
- 
+     #Día 5
+    time_cinco_prev = prev_dias[6]["time"]
+    #convertimos UNIX recibido a formato legible
+    print(datetime.utcfromtimestamp(time_cinco_prev).strftime('%d / %m'))
+
+    temp_max_cinco = int(prev_dias[6]["temperatureHigh"])
+    temp_min_cinco = int(prev_dias[6]["temperatureLow"])
+    pres_cinco = prev_dias[6]["pressure"]
+    icon_cinco = prev_dias[6]["icon"]
+
+    #mostramos un icono para saber si la presión sube o baja respecto al día anterior
+    if pres_cinco > pres_cuatro:
+        print(r'\u1F815')
+    elif pres_cinco == pres_cuatro:
+        print('=')
+    else:
+        print(r'\u1F817')
